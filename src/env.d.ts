83fe62e80d659d4432726c366a110dac8c53b3b3
20190225001 <839492148@qq.com>
2025-06-16 02:02:27 +0800

    feat: add types/node

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-
+declare const __non_webpack_require__: NodeRequire
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
